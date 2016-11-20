d531a92b8884c0472f79692362ffc0a96a1cd6a5
Carlos Benavides V <carlos.benavides@digitaslbi.com>
2016-11-20 09:40:26 -0600

    Modal added

--- app/assets/scripts/core/modules.js
+++ app/assets/scripts/core/modules.js
@@ -8,6 +8,7 @@ module.exports = {
  	header: 	require('../../../components/header/header.js'),
  	footer: 	require('../../../components/footer/footer.js'),
  	w3cFetcher: 	require('../../../components/w3c-fetcher/w3c-fetcher.js'),
+ 	urlsModal: 	require('../../../components/urls-modal/urls-modal.js'),
 
 
 
--- dist/assets/scripts/bundle.js
+++ dist/assets/scripts/bundle.js
@@ -52,6 +52,7 @@ module.exports = {
  	header: 	require('../../../components/header/header.js'),
  	footer: 	require('../../../components/footer/footer.js'),
  	w3cFetcher: 	require('../../../components/w3c-fetcher/w3c-fetcher.js'),
+ 	urlsModal: 	require('../../../components/urls-modal/urls-modal.js'),
 
 
 
@@ -59,7 +60,7 @@ module.exports = {
 
 }
 
-},{"../../../components/footer/footer.js":5,"../../../components/header/header.js":6,"../../../components/w3c-fetcher/w3c-fetcher.js":7,"./pubsub.js":4}],4:[function(require,module,exports){
+},{"../../../components/footer/footer.js":5,"../../../components/header/header.js":6,"../../../components/urls-modal/urls-modal.js":7,"../../../components/w3c-fetcher/w3c-fetcher.js":8,"./pubsub.js":4}],4:[function(require,module,exports){
 
 var events = {};
 
@@ -142,6 +143,78 @@ module.exports = {
 	autoLunch: false
 };
 },{"../../assets/scripts/core/pubsub.js":4}],7:[function(require,module,exports){
+const pubsub = require('../../assets/scripts/core/pubsub.js');
+
+// Global Variables
+let modalUrl    = $('.modal-url');
+let btnAdd      = modalUrl.find('.btn-add-field');
+let btnSaveUlrs = modalUrl.find('.btn-save-url');
+let urlsSection = modalUrl.find('.input-append');
+let doc         = $(document);
+
+
+let buttonsActions = {
+    addField: () => {
+        let data = `<div class="field-wrapper">
+                        <input autocomplete="off" class="input-url form-control" placeholder="URL to test" type="text">
+                        <a href="#" class="btn btn-danger remove-me" >
+                            -
+                        </a>
+                    </div>`
+        urlsSection.append(data);
+    },
+    removeField: (item) => {
+        item.parent().remove();
+    },
+    saveUrls: () => {
+        let urls = handleUrls.getUrlsFromForm();
+
+    }
+};
+let handleUrls = {
+
+    getUrlsFromForm: () => {
+        urlsSection.find('.input-url').each((i,item) => {
+            // console.log(e)
+            console.log(item.value)
+        })
+        // console.log(urls);
+    },
+    areUrlsValid: () => true  // needs to add a real validation here
+
+}
+
+
+// bind events to DOM
+const bindEventsToUI = () => {
+    btnAdd.on('click', (e) => {
+        e.preventDefault();
+        buttonsActions.addField();
+    });
+    doc.on('click', '.remove-me', (e) => {
+        buttonsActions.removeField($(e.target));
+    });
+    btnSaveUlrs.on('click', (e) => {
+        buttonsActions.saveUrls();
+    })
+};
+
+// public interface
+const init = function() {
+    console.log('w3c-fetcher'); 
+    bindEventsToUI();
+};
+
+
+// export public interface
+module.exports = {
+    init:init,
+    autoLunch: true
+};
+
+
+
+},{"../../assets/scripts/core/pubsub.js":4}],8:[function(require,module,exports){
 const pubsub = require('../../assets/scripts/core/pubsub.js');
 
 
